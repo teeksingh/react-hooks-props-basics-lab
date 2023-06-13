b9419e9b1516803ba1ffb0efeb84dcfb202d03fd
Prateek <teeksingh@gmail.com>
2023-06-12 21:32:58 -0500

    Completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p></p>
+      {props.bio !== ""&& props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={props.github} linkedin={props.linkedin}/>
       
